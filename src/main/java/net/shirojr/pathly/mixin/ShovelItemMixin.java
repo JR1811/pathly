99106891c97dce3beff75212e526d6a4dd1e8678
JR1811 <janreichelt77@gmail.com>
2026-08-02 02:18:11 +0200

    added support for taller replaceable path top blocks, see issue #3

--- src/main/java/net/shirojr/pathly/mixin/ShovelItemMixin.java
+++ src/main/java/net/shirojr/pathly/mixin/ShovelItemMixin.java
@@ -12,18 +12,25 @@ import net.minecraft.item.ShovelItem;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.shirojr.pathly.init.PathlyGamerules;
 import net.shirojr.pathly.init.PathlyTags;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Mixin(ShovelItem.class)
 public abstract class ShovelItemMixin extends MiningToolItem {
+    @Unique
+    private static final int PATH_TOP_REPLACEABLE_SCAN_DEPTH = 3;
+
     @Shadow
     @Final
     protected static Map<Block, BlockState> PATH_STATES;
@@ -37,21 +44,37 @@ public abstract class ShovelItemMixin extends MiningToolItem {
                                          @Local(argsOnly = true) ItemUsageContext context,
                                          @Local LocalRef<BlockPos> pos,
                                          @Local LocalRef<BlockState> state) {
-        BlockState originalState = state.get();
-        BlockPos originalPos = pos.get();
-        BlockState originalPathState = PATH_STATES.get(originalState.getBlock());
-        if (originalPathState != null) return originalPathState;
-        if (!context.getWorld().getGameRules().getBoolean(PathlyGamerules.ENABLE_PATH_TOP_REPLACABLES)) return null;
+        if (!context.getWorld().getGameRules().getBoolean(PathlyGamerules.ENABLE_PATH_TOP_REPLACABLES)) {
+            return original.call(instance, o);
+        }
         World world = context.getWorld();
-        BlockPos posBelow = originalPos.down();
-        BlockState stateBelow = world.getBlockState(posBelow);
-        BlockState pathStateBelow = PATH_STATES.get(stateBelow.getBlock());
-        if (pathStateBelow == null) return null;
-        if (!originalState.isReplaceable() && !originalState.isIn(PathlyTags.BlockTags.PATH_TOP_REPLACABLES))
-            return null;
-        world.breakBlock(originalPos, true, context.getPlayer());
-        pos.set(posBelow);
-        state.set(stateBelow);
-        return pathStateBelow;
+        BlockPos.Mutable posWalker = pos.get().mutableCopy();
+        List<BlockPos> toBeBroken = new ArrayList<>();
+
+        for (int i = 0; i < PATH_TOP_REPLACEABLE_SCAN_DEPTH; i++) {
+            BlockState blockState = world.getBlockState(posWalker);
+            BlockState pathState = PATH_STATES.get(blockState.getBlock());
+            if (pathState != null) {
+                toBeBroken.forEach(posEntry ->
+                        world.breakBlock(posEntry, true, context.getPlayer())
+                );
+                pos.set(posWalker.toImmutable());
+                state.set(blockState);
+                return pathState;
+            }
+            if (!isPathTopReplaceable(blockState)) {
+                return null;
+            }
+            toBeBroken.add(posWalker.toImmutable());
+            posWalker.move(Direction.DOWN);
+        }
+
+        return null;
+    }
+
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
+    @Unique
+    private static boolean isPathTopReplaceable(BlockState state) {
+        return state.isReplaceable() || state.isIn(PathlyTags.BlockTags.PATH_TOP_REPLACABLES);
     }
 }
